f9a383412cee389f8d7c2d5c6ccb320e682fb488
Ckamara1998 <144286684+Ckamara1998@users.noreply.github.com>
2024-01-23 15:05:14 +0200

    SAVE

---
+++ animation.js
@@ -0,0 +1,8 @@
+
+function openNav() {
+    document.getElementById("menu-one").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("menu-one").style.width = "0";
+  }
